7c036bb8bc5f63bc50c7ced0cd6a47f5105bd346
Gracious Nguwo <graciousnguwo2020@gmail.com>
2022-09-13 13:48:33 +0200

    added a sidebar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Paper, IconButton } from '@mui/material';
-import {SearchIcon} from '@mui/icons-material';
+import {Search} from '@mui/icons-material';
 
 export const SearchBar = () => {
   return (
     <Paper component="form" onSubmit={() => {}} sx={{borderRadius: 20, border: '1px solid #e3e3e3', pl:3, boxShadow: 'none', mr: {sm: 5}}}>
-        <input className='search-bar' placeholde="Search..." value="" onChange={()=> {}}/>
+        <input className='search-bar' placeholder="Search..." value="" onChange={()=> {}}/>
         <IconButton>
-            <SearchIcon />
+            <Search />
         </IconButton>
         
         
--- src/utils/constrants.js
+++ src/utils/constrants.js
@@ -12,7 +12,8 @@ import TheaterComedyIcon from '@mui/icons-material/TheaterComedy';
 import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
 import DeveloperModeIcon from '@mui/icons-material/DeveloperMode';
 
-export const logo = 'https://i.ibb.co/s9Qys2j/logo.png';
+export const logo1 = 'https://i.ibb.co/s9Qys2j/logo.png';
+export const logo = 'https://thumbs.dreamstime.com/b/vector-design-mobile-application-logo-youtube-apk-icon-118452871.jpg'
 
 export const categories = [
   { name: 'New', icon: <HomeIcon />, },

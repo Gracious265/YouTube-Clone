d00e934e20d83c35e09cecdb280e72cf89d28133
Gracious Nguwo <graciousnguwo2020@gmail.com>
2022-09-17 15:10:09 +0200

    added card detail compontent

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import { Stack, Box} from '@mui/material'
-import VideoCard from './VideoCard'
-import ChannelCard from './ChannelCard'
+import {VideoCard, ChannelCard} from './'
+
 
 
 const Videos = ({videos}) => {
-   
+   console.log(videos)
   return (
     <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2} >
         {videos.map((item, idx) => (
-
+        
         <Box key={idx}>
-            {item.id.videoId && <VideoCard video={item} />} 
-            {item.id.channelId && <ChannelCard channelDetail={item} />} 
+          {item.id.videoId && <VideoCard video={item} /> }
+          {item.id.channelId && <ChannelCard channelDetail={item} />}
         </Box>
 
         ))}
